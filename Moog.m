074e2ce0b7baa1f1a2fb66a2bcfb20e0b7cfcc02
Michael Castanieto <michaelcastanieto@h439.aau1x.klient.sydhavn.site.aau.dk>
2016-12-25 19:42:03 +0100

    cleaned up comments, added amplitude parameter

--- Moog.m
+++ Moog.m
@@ -1,58 +1,33 @@
 classdef Moog < audioPlugin
     % Moog
-    %           The effect delays the input signal from 0 - 1 second.
-    %           Works in real-time and can generate an audio-plugin
-    %           by using built-in functions from audio system toolbox
+    %           The Moog class is a real-time implementation of the 
+    %           Moog ladder filter. This implementation is based on 
+    %           Huovilainen's 2004 paper 'Non-Linear Digital Implementation
+    %           of the Moog Ladder filter'
     % Input
-    %           Delay: returns the delaytime in seconds
-    %           Gain: Amplitude of the delayed signal, 0-1
-    %           Feedback: how much of the delayed signal is feeded back into the
-    %           effect. This should not be over 0.9 due to stability.
-    %           Wet/Dry: How much of the original signal (dry ) and
-    %           delayed signal (wet) is present in the out. Can mixed between 0-1.
-    %           At 0 only the dry signal is present, at 1 the output is completely wet.
-    % Effects
-    
-    %           Vibrato:
-    %           Adds vibrato to the delayed signal. Can be controlled through
-    %           'Vibrate Rate' and 'Vibrato Depth'
-    %
-    %           Reverse:
-    %           Reverses the delayed signal.
-    %
-    %           Saturation:
-    %           Distorts the delayed signal. The amount of distortion can be
-    %           controlled with 'Saturation Amount'
-    %
-    %           HighPass and LowPass Filter:
-    %           You can add a highpass or a lowpass filter to the delayed
-    %           signal. The cutoff frequency can be controlled with the parameter Fc, and the
-    %           quality of the filter can be controlled with Q.
-    %
-    %           Effects to implemented: Reverb?, grainular
-    %           delay.
+    %           A: The amplitude gain of the filter's output
+    %           Fc: The filter's cutoff frequency 
+    %           R: The filter's resonance
     properties
 	fc = 5000;
 	r = 0.5;
+	A = 0.5;
 	g
-	Vt = 2000000; % i chose a rediculously large number here to match
+	Vt = 2000000; % Thermal Voltage, value should technically be 25 mV
+	              % I chose a rediculously large number here to match
 	              % values that are similar to the algorithm i found online
+	% unit delays
 	yprev = [0 0;0 0;0 0;0 0;0 0;0 0];
 	Wprev = [0 0;0 0;0 0];
     end
     
     properties (Dependent)
-        %FeedbackLevel Feedback gain
-        %   Specify the feedback gain value as a positive scalar. This
-        %   value must range from 0 to 0.5. Setting FeedbackLevel to 0
-        %   turns off the feedback. The default value of this property is
-        %   0.35.
-        FeedbackLevel = 0.35
-        
     end
     
     properties (Constant)
-        tTable = tanh(linspace(-1e-10,1e-10,2000000));
+
+        %tTable = tanh(linspace(-1e-10,1e-10,2000000));
+
         % audioPluginInterface manages the number of input/output channels
         % and uses audioPluginParameter to generate plugin UI parameters.
         PluginInterface = audioPluginInterface(...
@@ -62,6 +37,8 @@ classdef Moog < audioPlugin
             'VendorName', '', ...
             'VendorVersion', '3.1.4', ...
             'UniqueId', '4pvz',...
+            audioPluginParameter('A',...
+            'DisplayName','Amplitude','Label','Amp','Mapping',{'lin',0 1}),...
             audioPluginParameter('fc',...
             'DisplayName','Cutoff Frequency','Label','Fc','Mapping',{'lin',0 10000}),...
             audioPluginParameter('r',...
@@ -69,7 +46,7 @@ classdef Moog < audioPlugin
     end
     
     properties (Access = private)
-        %pSR Sample rate
+        % Sample rate
 	fs
     end
     
@@ -80,7 +57,7 @@ classdef Moog < audioPlugin
 	    obj.g = 1-exp(-2*pi*obj.fc/obj.fs);
         end
         
-        % resets internal states of buffers
+        % reset parameter values and internal buffers
         function reset(obj)
             % reset sample rate
 	    fs = getSampleRate(obj);
@@ -96,6 +73,10 @@ classdef Moog < audioPlugin
 	    obj.g = 1-exp(-2*pi*fc/fs);
         end
         
+        function set.A(obj, A)
+            obj.A = A;
+        end
+
         function set.fc(obj, fc)
             obj.fc = fc;
 	    obj.g = 1-exp(-2*pi*fc/obj.fs);
@@ -105,12 +86,12 @@ classdef Moog < audioPlugin
             obj.r = r;
         end
 
-	function y = tanhTable(x)
-	    y = tTable(round(100*(x+1)+1));
-	end
+	%function y = tanhTable(x)
+	%    y = tTable(round(100*(x+1)+1));
+	%end
 
 	function [obj, out] = moogfilter(obj, x)
-	    A = max(x);
+	    A = max(x)*obj.A;
 	    out = [];
 	    yprev = obj.yprev;
 	    y = zeros(size(yprev));
@@ -123,7 +104,7 @@ classdef Moog < audioPlugin
 	    for n = 1:length(x)
 
 		    % number of times the moog ladder is processed, choosing 1 for now
-		    % (choosing 2 made it really glitchy)
+		    % (choosing m = 1:2 made it really glitchy)
 		    for m = 1:1
 			z = (x(n,:)-4*r*yprev(6,:))./(2*Vt);
 			%z = round(z*1e13)+1e6;
@@ -169,8 +150,8 @@ classdef Moog < audioPlugin
 		end
 		out = [out;yprev(6,:)];
 	    end
-	    maxout = max(out);
-	    out = A(1)*out/maxout(1);
+	    %maxout = max(out);
+	    out = A(1)*out/max(max(out));
 	    obj.yprev = yprev;
 	    obj.Wprev = Wprev;
 	end
